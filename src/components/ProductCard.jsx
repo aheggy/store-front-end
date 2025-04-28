2900d2b01b61ab5afb20c4dfbcb5a3fdc49de240
Ahmed Heggy <ahmedheggy@pursuit.org>
2025-04-28 15:44:38 -0400

    update script section with start: vite preview --port 10000

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -21,7 +21,7 @@ export default function ProductCard({ product }) {
         <div className="absolute inset-0 flex items-end justify-center">
           <button 
             onClick={handleViewDetails}
-            className="mb-4 bg-blue-700 hover:bg-blue-500 text-white font-semibold py-1.5 px-5 rounded-full opacity-0 translate-y-10 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-100 text-sm"
+            className="mb-0 bg-blue-700 hover:bg-blue-500 text-white font-semibold py-1.5 px-5 rounded-full opacity-0 translate-y-10 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-100 text-sm"
           >
             View Details
           </button> 
@@ -30,8 +30,8 @@ export default function ProductCard({ product }) {
 
       {/* Product Info */}
       <div className="p-4">
-        <h2 className="text-lg font-bold text-gray-800">{product.name}</h2>
-        <p className="text-gray-600 mt-1">${product.price.toFixed(2)}</p>
+        <h2 className="text-lg font-serif text-gray-800">{product.name}</h2>
+        <p className="font-serif text-gray-600 mt-1">${product.price.toFixed(2)}</p>
       </div>
     </div>
   )
